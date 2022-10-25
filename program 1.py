c2721e51206d243f35867d71a15311aabc01f6eb
Vinaykumar7599 <vinaypradeep003@gmail.com>
2022-10-25 12:31:01 +0530

    Delete program 1.py

--- program 1.py
+++
@@ -1,6 +0,0 @@
-import pypyodbc
-import config
-
-def getConnection():
-     connection = pypyodbc.connect("Driver= {"+config.DATABASE_CONFIG["Driver"]+"} ;Server=" + config.DATABASE_CONFIG["Server"] + ";Database=" + config.DATABASE_CONFIG["Database"] + ";uid=" + config.DATABASE_CONFIG["UID"] + ";pwd=" + config.DATABASE_CONFIG["Password"])
-     return connection
